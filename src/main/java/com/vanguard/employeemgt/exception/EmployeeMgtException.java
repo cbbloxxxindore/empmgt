bcb7a295c041edde9bd86b4c0153598e9db73ce3
abhijeet <abhijeet.thakur1@gmail.com>
2023-07-09 19:23:08 +0530

    add exception classes

---
+++ src/main/java/com/vanguard/employeemgt/exception/EmployeeMgtException.java
@@ -0,0 +1,15 @@
+package com.vanguard.employeemgt.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class EmployeeMgtException {
+
+    @ExceptionHandler(EmployeeNotFoundException.class)
+    public ResponseEntity<String> handleEmployeeNotFoundException(EmployeeNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+}
--- src/main/java/com/vanguard/employeemgt/resource/EmployeeResource.java
+++ src/main/java/com/vanguard/employeemgt/resource/EmployeeResource.java
@@ -1,9 +1,13 @@
 package com.vanguard.employeemgt.resource;
 
+import com.vanguard.employeemgt.exception.EmployeeNotFoundException;
 import com.vanguard.employeemgt.models.Employee;
 import com.vanguard.employeemgt.service.EmployeeService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,19 +15,35 @@ import java.util.List;
 
 @RestController
 public class EmployeeResource {
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeResource.class);
+    public  static final String  CLASS_NAME= EmployeeResource.class.getName()  ;
+    private static final Logger log = LoggerFactory.getLogger(EmployeeResource.class);
+    private static final String SAVE_METHOD = "createEmployee";
+    private static final String EXCEPTION = " Exception occurred while saving Employee";
 
     @Autowired
     private EmployeeService employeeService;
 
     public EmployeeResource(EmployeeService employeeService) {
+
         this.employeeService = employeeService;
     }
 @PostMapping("/employees")
-    public Employee createEmployee(@RequestBody @Valid Employee employee) {
+    public ResponseEntity<Employee >createEmployee(@RequestBody @Valid Employee employee) {
+
+    StringBuilder msgBuilder= new StringBuilder(CLASS_NAME).append("::"+SAVE_METHOD);
+    try {
+        log.info(msgBuilder.append("Attemping to save employee  resource").toString());
 
 
-        return employeeService.saveEmployee(employee);
-    }
+
+        log.info(msgBuilder.append("sucessfully  saved employee ").toString());
+        return  new ResponseEntity(employeeService.saveEmployee(employee), HttpStatus.OK);
+
+    }catch (EmployeeNotFoundException e)
+    {
+        return new ResponseEntity(e.getMessage(), HttpStatus.CONFLICT);    }
+}
 @GetMapping("/employees/{name}")
     public Employee getEmployeeByName(@PathVariable String name) {
         return employeeService.getEmployeeByName(name);
